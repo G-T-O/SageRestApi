386deaa50406b194368a1028e587ed210d5b6eb4
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-10-30 11:35:36 +0100

    Generic repo for the models (client/invoice...)

---
+++ Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        int AddAsync(T entity);
+        Task<int> UpdateAsync(T entity);
+        Task<int> DeleteAsync(int id);
+    }
+}
