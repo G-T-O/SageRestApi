ff14e680f209495ede44ed8e336ccd2c5ef96cd5
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-10-31 18:53:41 +0100

    Update ClientsController.cs

--- SageRestApi/Controllers/ClientsController.cs
+++ SageRestApi/Controllers/ClientsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Interfaces.Repositories;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,18 +14,27 @@ namespace SageRestApi.Controllers
     [ApiController]
     public class ClientsController : ControllerBase
     {
+        private readonly IClientRepository _client;
+        public ClientsController(IClientRepository client)
+        {
+            _client = client;
+        }
         // GET: api/<ClientsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public Client Get()
         {
-            return new string[] { "value1", "value2" };
+            Client client = new Client()
+            {
+
+            };
+            return client;
         }
 
         // GET api/<ClientsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Client>> Get(string id)
         {
-            return "value";
+            return await _client.GetByIdAsync(id);
         }
 
         // POST api/<ClientsController>
