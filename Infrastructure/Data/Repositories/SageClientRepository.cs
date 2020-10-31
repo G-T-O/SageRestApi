0fd1ed7348928949959c266c702824e176c9f71f
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-10-31 18:54:18 +0100

    DI SageClient repo

--- Infrastructure/Data/Repositories/SageClientRepository.cs
+++ Infrastructure/Data/Repositories/SageClientRepository.cs
@@ -1,14 +1,19 @@
-﻿using Application.Interfaces.Repositories;
+﻿using Application.Interfaces.Access;
+using Application.Interfaces.Repositories;
 using Core.Entities;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data.Repositories
 {
     public class SageClientRepository : ISageClientRepository
     {
+        private readonly ISageAccess _sageAccess;
+        public SageClientRepository(ISageAccess sageAccess)
+        {
+            _sageAccess = sageAccess;
+        }
         public int AddAsync(Client entity)
         {
             throw new NotImplementedException();
@@ -24,11 +29,10 @@ namespace Infrastructure.Data.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<Client> GetByIdAsync(int id)
+        public async Task<Client> GetByIdAsync(string CT_Num)
         {
             throw new NotImplementedException();
         }
-
         public Task<int> UpdateAsync(Client entity)
         {
             throw new NotImplementedException();
