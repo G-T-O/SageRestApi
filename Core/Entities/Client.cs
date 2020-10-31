2f85387f709460ca71f736def855058395c51eb7
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-10-31 18:50:33 +0100

    Update Client.cs

--- Core/Entities/Client.cs
+++ Core/Entities/Client.cs
@@ -6,5 +6,23 @@ namespace Core.Entities
 {
     public class Client
     {
+        public string CT_Num { get; set; }
+        public string CT_Intitule { get; set; }
+        public string CT_Qualite { get; set; }
+        public string CT_Siret { get; set; }
+        public string Email { get; set; }
+        public string Portable { get; set; }
+        public string Telecopie { get; set; }
+        public string Telephone { get; set; }
+        public string CT_Identifiant { get; set; }
+        public int Civ { get; set; }
+
+        public string Adresse { get; set; }
+        public string Complement { get; set; }
+        public string CodePostal { get; set; }
+        public string Ville { get; set; }
+        public string Pays { get; set; }
+
+        public string Activated { get; set; } = "True";
     }
 }
