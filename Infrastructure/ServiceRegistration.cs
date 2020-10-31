04b7e3433a1504cc16e4805fe97f86072763fcb8
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-10-31 17:32:10 +0100

    Injecting the dependencies

--- Infrastructure/ServiceRegistration.cs
+++ Infrastructure/ServiceRegistration.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Application.Interfaces.Access;
+using Application.Interfaces.Repositories;
+using Core.Entities;
+using Infrastructure.Data.DbAccess;
+using Infrastructure.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +12,15 @@ namespace Infrastructure
 {
     public static class ServiceRegistration
     {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IDapperAccess, DapperAccess>();
+            services.AddTransient<ISageAccess, SageAccess>();
+
+            services.AddTransient<IClientRepository, ClientRepository>();
+            services.AddTransient<ISageClientRepository, SageClientRepository>();
+            services.AddTransient<ISageInvoiceRepository, SageInvoiceRepository>();
+            services.AddTransient<ISageOrderRepository, SageOrderRepository>();
+        }
     }
 }
