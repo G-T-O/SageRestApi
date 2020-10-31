852132e23e5e82e13e3f6f77aea39c1425f356e2
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-10-31 17:34:53 +0100

    Adding the connection string to the Dapper Access Database

--- Application/Interfaces/Access/IDapperAccess.cs
+++ Application/Interfaces/Access/IDapperAccess.cs
@@ -6,5 +6,6 @@ namespace Application.Interfaces.Access
 {
     public interface IDapperAccess
     {
+        public string GetConnectionString { get;}
     }
 }
--- Infrastructure/Data/DbAccess/DapperAccess.cs
+++ Infrastructure/Data/DbAccess/DapperAccess.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Interfaces.Access;
+using System;
 
 namespace Infrastructure.Data.DbAccess
 {
-    public class DapperAccess
+    public class DapperAccess : IDapperAccess
     {
+        private readonly string _connectionString;
+        public string GetConnectionString { get => _connectionString; }
+    public DapperAccess()
+        {
+            _connectionString = "Data Source=DESKTOP-OO9BUGL;Initial Catalog=S_QUALIFELEC;Integrated Security=True;MultipleActiveResultSets=True";
+            //TO DO Call the string from AppSettings
+        }
     }
 }
