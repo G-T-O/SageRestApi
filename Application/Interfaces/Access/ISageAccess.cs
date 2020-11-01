884f27f3fe535cbda9c5e6f00b88423943cf4814
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-11-01 21:12:10 +0100

    Interface for the data access to Sage

--- Application/Interfaces/Access/ISageAccess.cs
+++ Application/Interfaces/Access/ISageAccess.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Objets100cLib;
 
 namespace Application.Interfaces.Access
 {
     public interface ISageAccess
     {
+        public BSCIALApplication100c GetSageDatabase { get; }
+        public object DatabaseLock { get; };
+        public string GetUsername { get; }
+        public string GetUserPwd { get; }
+        public string GetDatabaseName { get; }
+        public string GetDatabaseInstance { get; }
     }
 }
