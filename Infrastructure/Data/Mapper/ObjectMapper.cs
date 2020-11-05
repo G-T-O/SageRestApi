4d3b3df302a07337209683ae407ed2ffa91a5e91
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-11-05 23:27:43 +0100

    static class for the mapping of dto to business and Sage Object

---
+++ Infrastructure/Data/Mapper/ObjectMapper.cs
@@ -0,0 +1,20 @@
+﻿using Core.Dto;
+using Application.Interfaces.Access;
+using Objets100cLib;
+
+namespace Infrastructure.Data.Mapper
+{
+    public static class ObjectMapper
+    {
+        public static IBOClient3 ClientToIboClient(Client client,ISageAccess sageAccess)
+        {
+           IBOClient3 sageClient = (IBOClient3) sageAccess.GetSageDatabase.CptaApplication.FactoryClient.Create();
+            sageClient.CT_Qualite = "SARL";
+           return sageClient;
+        }
+/*        public static Client IBOClientToClient(IBOClient3 client)
+        {
+
+        }*/
+    }
+}
