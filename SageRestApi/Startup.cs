c39f2de8a1ab60d5e16e2793c8b1669ec7fd8899
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-11-05 17:09:45 +0100

    Cleaning

--- SageRestApi/Startup.cs
+++ SageRestApi/Startup.cs
@@ -1,8 +1,6 @@
 using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
