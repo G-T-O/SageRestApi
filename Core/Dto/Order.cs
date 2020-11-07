2bdb35bc5b70d1cfbb1e44f8e2e6128c8137f0ed
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-11-07 23:27:16 +0100

    Update Order.cs

--- Core/Dto/Order.cs
+++ Core/Dto/Order.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Core.Dto
 {
     public class Order
     {
+        private List<Article> _articles;
+        public string CT_Num { get; set; }
+        public string OrderNum { get; set; }
+        public string QualCode { get; set; }
+        public string DO_Date { get; set; }
+        public List<Article> Articles
+        {
+            set { _articles = value; }
+            get { return _articles; }
+        }
     }
 }
