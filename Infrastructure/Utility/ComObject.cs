c9b5457ba772cf03693792d38b74e659804e84d4
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-11-07 23:14:45 +0100

    Creating utility for connecting and closing the sageDb

---
+++ Infrastructure/Utility/ComObject.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces.Access;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Utility
+{
+    public static class ComObject
+    {
+        public static bool Open(ISageAccess sageAccess)
+        {
+            try
+            {
+                sageAccess.GetSageDatabase.CompanyServer = sageAccess.GetDatabaseInstance;
+                sageAccess.GetSageDatabase.CompanyDatabaseName = sageAccess.GetDatabaseName;
+                sageAccess.GetSageDatabase.Loggable.UserName = sageAccess.GetUsername;
+                sageAccess.GetSageDatabase.Loggable.UserPwd = sageAccess.GetUserPwd;
+                sageAccess.GetSageDatabase.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public static bool Close(ISageAccess sageAccess)
+        {
+            try
+            {
+                sageAccess.GetSageDatabase.Close();
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
