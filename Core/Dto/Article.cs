2a819bd8bbf9e092ac1bc15dc0e80b446d2225fc
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-11-13 03:01:05 +0100

    Create Article.cs

---
+++ Core/Dto/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dto
+{
+   public class Article
+    {
+        public string AR_Ref { get; set; }
+        public string AR_Design { get; set; }
+        public double ArticleQte { get; set; }
+        public double AR_PrixTTC { get; set; }
+        public double MontantHT { get; set; }
+        public int Remise { get; set; }
+    }
+}
