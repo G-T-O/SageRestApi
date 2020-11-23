d8dde9e464f8be2c4375417932666813d81c9efe
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2020-11-23 11:06:12 +0100

    Update ClientRepository.cs

--- Infrastructure/Data/Repositories/ClientRepository.cs
+++ Infrastructure/Data/Repositories/ClientRepository.cs
@@ -34,7 +34,7 @@ namespace Infrastructure.Data.Repositories
         {
             using (var connection = new SqlConnection(_dapperAccess.GetConnectionString))
             {
-                return await connection.QueryFirstOrDefaultAsync<Client>("SELECT * FROM F_COMPTET WHERE CT_Num = @CT_Num", new { CT_Num });
+                return await connection.QueryFirstOrDefaultAsync<Client>("SELECT CT_Num FROM F_COMPTET WHERE CT_Num = @CT_Num", new { CT_Num });
             }
         }
 
