16afdb96011bbaf2b145a1a6e86c696c9a82ee78
TARI pierre-enzo <38909596+G-T-O@users.noreply.github.com>
2021-05-16 22:57:14 +0200

    Single controller for client and adding swagger documentation for Client Controller

--- SageRestApi/Controllers/ClientsController.cs
+++ SageRestApi/Controllers/ClientsController.cs
@@ -14,13 +14,41 @@ namespace SageRestApi.Controllers
     public class ClientsController : ControllerBase
     {
         private readonly IClientService _clientService;
-        public ClientsController(IClientService client)
+        private readonly ISageClientService _sageClientService;
+        public ClientsController(IClientService client,ISageClientService sageClientService)
         {
             _clientService = client;
+            _sageClientService = sageClientService;
         }
 
+        /// <summary>
+        /// Retrieve Sage client informations from a Sage Code
+        /// </summary>
+        /// <response code="200">Success while retrieving the client</response>
+        /// <response code="400">Error while retrieving the client</response>
+        /// <response code="401">Bad credentials</response>
+        /// <response code="500">Internal server error</response>
+        [SwaggerOperation(Tags = new[] { "Informations clients" })]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Client))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] // add model API error
+        [Produces(MediaTypeNames.Application.Json)]
+        [HttpGet]
+        public async Task<ActionResult<Client>> Get([Required] string sageCode)
+        {
+            return await _clientService.GetByIdAsync(sageCode);
+        }
 
-        // POST api/<ClientsController>
+        /// <summary>
+        /// Create a client and retrieve it's new Sage code
+        /// </summary>
+        /// <response code="200">Success while creating the client</response>
+        /// <response code="400">Error while creating the client</response>
+        /// <response code="401">Bad credentials</response>
+        /// <response code="500">Internal server error</response>
+        [SwaggerOperation(Tags = new[] { "Create a new client" })]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Client))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] // add model API error
+        [Produces(MediaTypeNames.Application.Json)]
         [HttpPost]
         public async Task<IActionResult> AddClient(Client client)
         {
@@ -29,14 +57,25 @@ namespace SageRestApi.Controllers
 
         // PUT api/<ClientsController>/5
         [HttpPut]
-        public async Task<IActionResult> Put(Client client)
+        public async Task<IActionResult> PutClient(Client client)
         {
             return Ok(await _clientService.UpdateAsync(client));
         }
 
-        // DELETE api/<ClientsController>/5
+        /// <summary>
+        /// Delete a client with a Sage code
+        /// </summary>
+        /// <response code="200">Success while deleting the client</response>
+        /// <response code="400">Error while deleting the client</response>
+        /// <response code="401">Bad credentials</response>
+        /// <response code="500">Internal server error</response>
+        [SwaggerOperation(Tags = new[] { "Delete a client" })]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Client))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] // add model API error
+        [Produces(MediaTypeNames.Application.Json)]
+        [HttpPost]
         [HttpDelete("{ct_Num}")]
-        public async Task<IActionResult> Delete(string ct_Num)
+        public async Task<IActionResult> DeleteClient(string ct_Num)
         {
             return Ok(await _clientService.DeleteAsync(ct_Num));
         }
